0922719b732520643444fd92d9cec81781fc4c6a
João Vitorr Freitas Wolf <joaovitorfreitaswolff1@outlook.com>
2025-06-16 23:53:45 +0000

    Criação do tabuleiro

--- desafio_batalhanaval.c
+++ desafio_batalhanaval.c
@@ -2,3 +2,11 @@
 
 int main() {
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    // 1. Criando o tabuleiro (matriz 10x10) e preenchendo com água (0)
+    int tabuleiro[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
